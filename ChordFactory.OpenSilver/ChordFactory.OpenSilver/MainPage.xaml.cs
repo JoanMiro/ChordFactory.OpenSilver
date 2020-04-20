7f3c09831644d7092f805aad1422228bc1c8f8e6
Gordon Mackie <gordon.mackie@vwfs.co.uk>
2020-04-20 17:05:58 +0100

    Restart - back to basics

--- ChordFactory.OpenSilver/ChordFactory.OpenSilver/MainPage.xaml.cs
+++ ChordFactory.OpenSilver/ChordFactory.OpenSilver/MainPage.xaml.cs
@@ -1,13 +1,12 @@
 ﻿namespace ChordFactory.OpenSilver
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
     using System.Windows.Controls;
 
     public partial class MainPage : Page
     {
-        private MusicData musicData;
-
         public MainPage()
         {
             this.InitializeComponent();
@@ -16,28 +15,7 @@
 
         private void MainPage_Loaded(object sender, System.Windows.RoutedEventArgs e)
         {
-            this.musicData = new MusicData();
-            this.DataContext = this.musicData;
-            this.ShowDiagnostics();
-        }
-
-        private void ShowDiagnostics()
-        {
-            Console.WriteLine($"Chords Count [{this.musicData.Chords.Count}]");
-            Console.WriteLine($"Scales Count [{this.musicData.Scales.Count}]");
-
-            Debug.WriteLine($"Chords Count [{this.musicData.Chords.Count}]");
-            Debug.WriteLine($"Scales Count [{this.musicData.Scales.Count}]");
-
-            //if (this.FindName("chordName") is TextBlock chordName)
-            //{
-            //    chordName.Text = $"Chords Count [{this.musicData.Chords.Count}]";
-            //}
-
-            //if (this.FindName("scaleName") is TextBlock scaleName)
-            //{
-            //    scaleName.Text = $"Scales Count [{this.musicData.Scales.Count}]";
-            //}
+            this.DataContext = new MusicData();
         }
     }
 }
